38c72173d53b55de3c4d37c86f8c6a834595576b
C00276685 <C00276685@10.40.5.170>
2022-09-27 12:12:35 +0100

    Small change

--- Lab01/src/Lab1/Lab1q1.java
+++ Lab01/src/Lab1/Lab1q1.java
@@ -9,16 +9,18 @@ public class Lab1q1
 { // begin class ThermTest
 	public static void main(String args[]) 
 	{ // being main method
-
+		double tempB = 0;
+		
 		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
-		Thermometer thermB = new Thermometer();		// Create another instance of our Thermometer class
+		Thermometer thermB = new Thermometer(10.0);	// Create another instance of our Thermometer class
 
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
 		
-		thermB.setCelsius(10.0);
-		System.out.println("Temp. of Thermometer B is " + thermB.getCelsius() );
+		
+		tempB = thermB.getCelsius();
+		System.out.println("Temp. of Thermometer B is " + tempB );
 		
 	} // end main
 } // end class ThermTest
